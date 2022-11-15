6de8753556076952be7956c0b20d761528115737
Vincent Toon <vincenttoon22@gmail.com>
2022-11-15 01:04:04 -0600

    files updated

--- client/src/sw.js
+++ client/src/sw.js
@@ -1,21 +1,21 @@
-import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate, CacheFirst } from "workbox-strategies";
-import { precacheAndRoute } from "workbox-precaching";
-import { CacheableResponsePlugin } from "workbox-cacheable-response";
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
+import { precacheAndRoute } from 'workbox-precaching';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
-// import expiration plugin
-import { ExpirationPlugin } from "workbox-expiration";
+// Import the expiration plugin
+import { ExpirationPlugin } from 'workbox-expiration';
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-const cacheName = "static-resources";
+const cacheName = 'static-resources';
 const matchCallback = ({ request }) => {
   console.log(request);
   return (
     // CSS
-    request.destination === "style" ||
-    // Javascript
-    request.destination === "script"
+    request.destination === 'style' ||
+    // JavaScript
+    request.destination === 'script'
   );
 };
 
@@ -34,16 +34,16 @@ registerRoute(
 // Register route for caching images
 // The cache first strategy is often the best choice for images because it saves bandwidth and improves performance.
 registerRoute(
-  ({ request }) => request.destination === "image",
+  ({ request }) => request.destination === 'image',
   new CacheFirst({
-    cacheName: "my-image-cache",
+    cacheName: 'my-image-cache',
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
       new ExpirationPlugin({
         maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
       }),
     ],
   })
