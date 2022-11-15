ea58ff9a0bcda12454fe02a6a701c1f54bac4944
Vincent Toon <vincenttoon22@gmail.com>
2022-11-15 00:01:19 -0600

    just forgot to update submit file dummy head

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,24 +1,27 @@
-import "./form";
-import { initdb, getDb, postDb, deleteDb } from "./database";
-import { fetchCards } from "./cards";
-import { toggleForm, clearForm } from "./form";
+// Import functions
+import {initdb, postDb, deleteDb, editDb} from './database';
+import {fetchCards} from './cards';
+import { toggleForm, clearForm } from './form';
 
+// Import CSS files
 import "../css/index.css";
 
-import { Tooltip, Toast, Popover } from "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+// Import Bootstrap
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Logo from "../images/logo.png";
-import Bear from "../images/bear.png";
-import Dog from "../images/dog.png";
+// Import images
+import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
 
-window.addEventListener("load", function () {
+// On load functionality
+window.addEventListener('load', function () {
   initdb();
   fetchCards();
-
-  document.getElementById("logo").src = Logo;
-  document.getElementById("bearThumbnail").src = Bear;
-  document.getElementById("dogThumbnail").src = Dog;
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
 });
 
 // Form functionality
@@ -27,12 +30,12 @@ const newContactButton = document.getElementById("new-contact");
 let submitBtnToUpdate = false;
 let profileId;
 
-newContactButton.addEventListener("click", (event) => {
-  toggleForm();
-});
+newContactButton.addEventListener('click', event => {
+ toggleForm()
+})
 
-form.addEventListener("submit", (event) => {
-  // Handle data
+form.addEventListener('submit', (event) => {
+  // handle the form data
   event.preventDefault();
   let name = document.getElementById("name").value;
   let phone = document.getElementById("phone").value;
@@ -43,23 +46,56 @@ form.addEventListener("submit", (event) => {
   if (submitBtnToUpdate == false) {
     postDb(name, email, phone, profile);
   } else {
-    fetchCards();
-    // Toggles the submit button back to POST functionality
-    submitBtnToUpdate = false;
-  }
 
+    // Obtains values passed into the form element
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+
+   // Calls the editDB function passing in any values from the form element as well as the ID of the contact that we are updating
+   editDb(profileId, name, email, phone, profile);
+
+   fetchCards();
+
+   // Toggles the submit button back to POST functionality
+   submitBtnToUpdate = false;
+  }
+  
   // Clear form
   clearForm();
   // Toggle form
   toggleForm();
   // Reload the DOM
   fetchCards();
-});
+})
 
+// Card functionality
+// Adds deleteCard() to the global scope so each card has access to it.
 window.deleteCard = (e) => {
+  // Grabs the id from the button element attached to the contact card.
     let id = parseInt(e.id);
-    // delete the card
+    // Delete the card
     deleteDb(id);
     // Reload the DOM
     fetchCards();
+  };
+
+window.editCard = (e) => {
+  // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+  profileId = parseInt(e.dataset.id);
+  
+  // Grabs information to pre-populate edit form
+  let editName = e.dataset.name;
+  let editEmail = e.dataset.email;
+  let editPhone = e.dataset.phone;
+  
+  document.getElementById("name").value = editName;
+  document.getElementById("email").value = editEmail;
+  document.getElementById("phone").value = editPhone;
+  
+  form.style.display = "block";
+  
+  // Toggles the submit button so that it now Updates an existing contact instead of posting a new one
+  submitBtnToUpdate = true;
 };
