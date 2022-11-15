89ec13ac12caf470545f76eb94d312dca056051e
Vincent Toon <vincenttoon22@gmail.com>
2022-11-15 00:57:02 -0600

    service worker added

--- client/src/js/form.js
+++ client/src/js/form.js
@@ -6,10 +6,10 @@ export const toggleForm = () => {
   } else {
     form.style.display = "none";
   }
-}
+};
 
 export const clearForm = () => {
   document.getElementById("name").value = "";
   document.getElementById("phone").value = "";
-  document.getElementById("email").value = "";   
-}
+  document.getElementById("email").value = "";
+};
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/js/index.js",
@@ -9,32 +10,34 @@ module.exports = {
   },
   module: {
     rules: [
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
         },
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['@babel/preset-env', {targets: 'defaults' }]
-                    ]
-                }
-            }
-        }
-      ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpack Plugin',
-    })
-  ]
+      template: "./index.html",
+      title: "Webpack Plugin",
+    }),
+    new InjectManifest({
+      swSrc: "./src/sw.js",
+      swDest: "service-worker.js",
+    }),
+  ],
 };
